92858b10515892bb6319db483e93c0e04d151571
thien-bach <thien-bach@hingehealth.com>
2018-09-02 18:18:23 -0700

    Add test cases for the anglicize method

--- anglicize.ts
+++ anglicize.ts
@@ -1,7 +1,7 @@
-export const ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-export const tens = ['ten', 'eleven', 'twelve', 'thirteen',
+export const ONES = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+export const TEENS = ['ten', 'eleven', 'twelve', 'thirteen',
                     'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-export const teens = ones.concat(tens);
+export const ONES_TEENS = ONES.concat(TEENS);
 
 /**
  * anglicizes passed integers
@@ -9,6 +9,6 @@ export const teens = ones.concat(tens);
  */
 export const anglicize = (n: number): string => {
     if (n < 1 || n > 999999) throw('n doesn\'t satisfy: 0 < n < 1,000,000');
-    if (n < 20) return teens[n];
+    if (n < 20) return ONES_TEENS[n];
     return '';
 }
